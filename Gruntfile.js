192bf21cd63dd8563886a35ca13a3ffac7b943cc
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-30 12:08:26 -0600

    added grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,12 +14,24 @@ module.exports = function(grunt) {
         },
     },
 
+    watch: {
+        scripts: {
+          files: ['**/*.scss'],
+          tasks: ['sass'],
+        },
+    }
+
   });
+
+// new tasks to checkout: jshint, concat, uglify
+
 
   // Load the grunt plugins
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', ['sass']);
+  grunt.registerTask('doubledown', ['sass', 'watch']);
 
 };
