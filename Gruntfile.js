7b180000e4d307c325b63ad3d714068272478186
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-30 15:54:52 -0600

    added jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,18 @@ module.exports = function(grunt) {
           files: ['**/*.scss'],
           tasks: ['sass'],
         },
+    },
+
+    concat: {
+      dist: {
+        src: ['src/foo.js', 'src/bar.js'],
+        dest: 'dist/output.js'
+      }
+    },
+
+    jshint: {
+      beforeconcat: ['src/foo.js', 'src/bar.js'],
+      afterconcat: ['dist/output.js']
     }
 
   });
@@ -29,6 +41,7 @@ module.exports = function(grunt) {
   // Load the grunt plugins
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
   grunt.registerTask('default', ['sass']);
---
+++ main.js
@@ -0,0 +1,4 @@
+let heads = document.querySelector('h1')
+heads.addEventListener('click', function() {
+  console.log('this is a header')
+})
